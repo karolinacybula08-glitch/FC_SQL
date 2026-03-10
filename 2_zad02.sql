a084c31dc63ba9e49d58599b2cf7856f4f801dae
karolinacybula08-glitch <karolina.cybula08@gmail.com>
2026-03-10 16:04:09 +0100

    Update 2_zad02.sql

--- 2_zad02.sql
+++ 2_zad02.sql
@@ -3,6 +3,31 @@ USE pracownicy;
 ALTER TABLE pracownicy
 RENAME COLUMN nr TO id;
 
+TRUNCATE TABLE pracownicy;
+
+INSERT INTO pracownicy (imię, nazwisko, wiek, kurs)
+VALUES
+("Anna", "NOWAK", 34, "DS."),
+("Roman", "KOWALSKI", 42, "DS."),
+("Tomasz", "WIŚNIEWSKI", 33, "DS."),
+("Agata", "WÓJCIK", 43, "DS."),
+("Elżbieta", "KOWALCZYK", 28, "Java"),
+("Przemysław", "", 34, "Java"),
+("Robert", "", 35, "Java"),
+("Radosław", "ZIELIŃSKI", 38, "Java"),
+("", "WOŹNIAK", 26, "Java"),
+("Robert", "SZYMAŃSKI", 34, "Java"),
+("Radosław", "DĄBROWSKI", 35, "UX"),
+("Robert", "KOZŁOWSKI", 38, "UX"),
+("Joanna", "MAZUR", 26, "UX"),
+("Radosław", "JANKOWSKI", 27, "UX"),
+("Patryk", "LEWANDOWSKI", 28, "Tester"),
+("", "ZIELIŃSKI", 28, "Tester"),
+("Andrzej", "WOŹNIAK", 31, "Tester"),
+("Andrzej", "LEWANDOWSKI", 30, "Tester"),
+("Roman", "", 39, "Tester"),
+("Ewa", "WOŹNIAK", 31, "Tester");
+
 SELECT *
 FROM pracownicy
 WHERE wiek = 28;
